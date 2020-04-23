4005a8cddf6c869e7c505c8bd1e06625f1c55b23
wjbaker101 <willbaker10198@gmail.com>
2020-04-23 12:46:49 +0100

    Cleanup main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import Vue from 'vue';
+
 import App from '@/App.vue';
-
 import { appRouter as router } from '@/router/appRouter';
-import { appStore } from '@/store/appStore';
+import { appStore as store } from '@/store/appStore';
 
-import './registerServiceWorker';
+import '@/registerServiceWorker';
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
-    store: appStore,
+    store,
     render: h => h(App),
 
     beforeCreate() {
